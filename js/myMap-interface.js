45e4c52e0041d565749e0efdb8bf1a9fd019fbf8
Talayruk <notejde@gmail.com>
2016-10-13 11:53:22 -0700

    add mapDemo

---
+++ js/myMap-interface.js
@@ -0,0 +1,1 @@
+var MyMap = require('./../js/myMap.js').myMapModule;
---
+++ js/myMap.js
@@ -0,0 +1,11 @@
+var apiKey = require('./../.env').apiKey;
+
+// function Calculator(constructorParameter) {
+//   // constructor
+// }
+//
+// MyMap.prototype. = function(methodParameter) {
+//   // method code
+// }
+//
+// exports.myMapModule = MyMap;
